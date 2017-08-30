91d3b0471933fd2fe552cc52ffd94b2c6501d8d1
Tomohito Ozaki <ozaki@yuroyoro.com>
2017-08-30 14:06:59 +0900

    [rhc] string.c : added String#palindrome?

--- string.c
+++ string.c
@@ -9457,6 +9457,27 @@ rb_str_is_ascii_only_p(VALUE str)
 }
 
 /**
+ *  call-seq:
+ *     str.palindrome?  -> true or false
+ *
+ *  Returns true for a string is palindrome.
+ *
+ *    "Sator Arepo Tenet Opera Rotas".palindrome? #=> true
+ *    "ab".palindrome? #=> false
+ */
+
+static VALUE
+rb_str_palindrome_p(VALUE self)
+{
+    const char *pat = "[^A-z0-9\\p{hiragana}\\p{katakana}]";
+    VALUE argv[2] = {rb_reg_regcomp(rb_utf8_str_new_cstr(pat)),
+                     rb_str_new_cstr("")};
+    VALUE filtered_str = rb_str_downcase(0, NULL, str_gsub(2, argv, self, FALSE));
+    return rb_str_empty(filtered_str) ? Qfalse:
+           rb_str_equal(filtered_str, rb_str_reverse(filtered_str));
+}
+
+/**
  * Shortens _str_ and adds three dots, an ellipsis, if it is longer
  * than _len_ characters.
  *
@@ -10561,6 +10582,7 @@ Init_String(void)
     rb_define_method(rb_cString, "b", rb_str_b, 0);
     rb_define_method(rb_cString, "valid_encoding?", rb_str_valid_encoding_p, 0);
     rb_define_method(rb_cString, "ascii_only?", rb_str_is_ascii_only_p, 0);
+    rb_define_method(rb_cString, "palindrome?", rb_str_palindrome_p, 0);
 
     /* define UnicodeNormalize module here so that we don't have to look it up */
     mUnicodeNormalize          = rb_define_module("UnicodeNormalize");
