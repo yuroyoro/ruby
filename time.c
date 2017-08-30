2aeb29f8a660dcccb1e29c3c4e2c7e510873d327
Tomohito Ozaki <ozaki@yuroyoro.com>
2017-08-30 15:13:08 +0900

    [rhc] time.c : added Time#day_before

--- time.c
+++ time.c
@@ -4471,6 +4471,22 @@ time_strftime(VALUE time, VALUE format)
     }
 }
 
+static VALUE
+time_day_before(int argc, VALUE *argv, VALUE self)
+{
+    VALUE nth;
+    int n, sec, day_before_sec;
+
+    rb_scan_args(argc, argv, "01", &nth);
+    if (nth == Qnil) nth = INT2FIX(1);
+    n = NUM2INT(nth);
+
+    sec = NUM2INT(time_to_i(self));
+    day_before_sec = sec - (60 * 60 * 24 * n);
+
+    return rb_funcall(rb_cTime, rb_intern("at"), 1, INT2NUM(day_before_sec));
+}
+
 /* :nodoc: */
 static VALUE
 time_mdump(VALUE time)
@@ -4895,6 +4911,8 @@ Init_Time(void)
     rb_define_method(rb_cTime, "subsec", time_subsec, 0);
 
     rb_define_method(rb_cTime, "strftime", time_strftime, 1);
+
+    rb_define_method(rb_cTime, "day_before", time_day_before, -1);
 
     /* methods for marshaling */
     rb_define_private_method(rb_cTime, "_dump", time_dump, -1);
